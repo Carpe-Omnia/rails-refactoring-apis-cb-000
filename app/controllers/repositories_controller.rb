8f6eed8be9dcbdd3dd5f71f67544c5341fd8a14f
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-04 02:58:36 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,10 @@
 class RepositoriesController < ApplicationController
   def index
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    @repos_array = JSON.parse(response.body)
+    hash = {} 
+    hash["name"] = "index" 
+    hash["html_url"] = "stub" 
+    repo = GithubRepo.new(hash)
+    @repos_array = repo.index()
   end
 
   def create
